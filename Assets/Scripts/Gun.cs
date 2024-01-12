c42117dd439b80597051bc2fa2efe4980324e879
Albert Martinez <amartineztre@gmail.com>
2024-01-12 17:02:26 +0100

    Gun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    [SerializeField] private BasicBulletBehaviour projectilePrefab;
     [SerializeField] private Transform shootPosition;
     private SpriteRenderer spriteRenderer;
     private int current = 0;
-    private float WPradius = 0.1f;
+    private float WPradius = 0.01f;
     public float speed;
 
+    private int bulletIndex = 0;
+
+    private int bullets = 6;
+
+    private bool canShoot = true;
+
+    private Animator animator;
+
     [SerializeField] private List<GameObject> waypoints = new List<GameObject>();
+
+    [SerializeField] private List<BasicBulletBehaviour> bulletType = new List<BasicBulletBehaviour>();
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         Random.seed = System.DateTime.Now.Millisecond;
     }
@@ -24,30 +34,70 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var pos = Camera.main.WorldToScreenPoint(transform.position);
-        var dir = Input.mousePosition - pos;
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (mousePos.x > transform.position.x)
+        if (!animator.GetBool("Reloading"))
         {
-            spriteRenderer.flipY = false;
-        }
-        else if(mousePos.x < transform.position.x)
-        {
-            spriteRenderer.flipY = true;
-        }
+            var pos = Camera.main.WorldToScreenPoint(transform.position);
+            var dir = Input.mousePosition - pos;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x > transform.position.x)
+            {
+                spriteRenderer.flipY = false;
+            }
+            else if (mousePos.x < transform.position.x)
+            {
+                spriteRenderer.flipY = true;
+            }
+        }
+        
         if (Input.GetMouseButtonDown(0))
         {
-            BasicBulletBehaviour projectile = Instantiate(projectilePrefab, shootPosition.position, transform.rotation);
-            projectile.LaunchProjectile(transform.right);
+
+            if (bullets > 0 && canShoot)
+            {
+                canShoot = false;
+                BasicBulletBehaviour projectile = Instantiate(bulletType[bulletIndex], shootPosition.position, transform.rotation);
+                projectile.LaunchProjectile(transform.right);
+                animator.Play("GunShoot");
+                bullets--;
+                StartCoroutine(ShootDelay());
+                
+                //SONIDO DE DISPARO
+            }
+            else
+            {
+                //SONIDO DE ARMA SIN BALA(??)
+            }
+
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !animator.GetBool("Reloading") && bullets != 6)
+        {
+            canShoot = false;
+            animator.SetBool("Reloading", true);
+            StartCoroutine(Reload());
+            bullets = 6;
         }
 
         MoveBetweenWaypoints();
 
+    }
+
+    IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(0.25f);
+        canShoot = true;
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(3f);
+        canShoot = true;
+        animator.SetBool("Reloading", false);
     }
 
     private void MoveBetweenWaypoints()
